5365c30393875756260f7a01b5f709360d0e47cd
Shaun van Ardenne <s.vanardenne@gmail.com>
2021-02-18 12:07:05 -0800

    Added basic footer

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Home from './components/Home';
 import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 // Higher-order component for providing context consumer
 import withContext from './Context';
@@ -38,6 +39,7 @@ function App() {
           <Route path="/projects" component={ProjectsWithContext} />
           <Route path="/contact" component={Contact} />
         </Switch>
+        <Footer />
       </div>
     </BrowserRouter>
 
