e35d60b8453bf65925f6d975cffb9855dc3da01d
Shaun van Ardenne <s.vanardenne@gmail.com>
2021-02-24 14:59:25 -0800

    Added hover effect on project cards.  Need to fix layout still.

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -7,13 +7,14 @@ const ProjectCard = (props) => {
 
   return(
     <div className="card-wrapper">
-      <img 
+      <img
+        className="project-img" 
         src={require(`../img/projects/${project.projectImage}`).default} 
         alt={project.projectTitle}
       />
       <div className="project-info">
         <h3 className="project-title">{project.projectTitle}</h3>
-        <div onClick={() => props.handleModal(project.id)} className="button">More info</div>
+        <div onClick={() => props.handleModal(project.id)} className="button project-button">More info</div>
       </div>
     </div>
   );
