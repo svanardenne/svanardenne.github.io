9f52f07fc900fdf2738c5f22724af241c093a9d3
Shaun van Ardenne <s.vanardenne@gmail.com>
2021-03-01 13:12:56 -0800

    Changed Contact.js from function to class

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,36 +1,38 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 
-const Contact = (props) => {
+class Contact extends Component {
 
   // Sets state for controlled inputs
-  const [ inputValue, setInputValue ] = useState({
+  state={
     name: "",
     email: "",
-    message: ""
-  });
+    message: "",
+  }
 
   // Handles value change in input fields
-  const handleChange = (e) => {
+  handleChange = (e) => {
     const name = e.target.name;
     const value = e.target.value;
-    setInputValue({
+    this.setState({
       [name]: value
     });
   }
-
-  return(
-    <section id="contact" className="main-content contact-wrapper">
-    <h2 className="contact-title">Contact</h2>
-      <div className="form-wrapper">
-        <form className="contact-form">
-          <input onChange={handleChange} defaultValue={inputValue.name} type="text" name="name" placeholder="Name" />
-          <input onChange={handleChange} defaultValue={inputValue.email} type="email" name="email" placeholder="Enter Email" />
-          <textarea onChange={handleChange} defaultValue={inputValue.message} type="text" name="message" placeholder="Your Message" />
-          <button type="submit" className="button form-button">Submit</button>
-        </form>
-      </div>
-    </section>
-  );
+  render() {
+    return(
+      <section id="contact" className="main-content contact-wrapper">
+      <h2 className="contact-title">Contact</h2>
+        <div className="form-wrapper">
+          <form className="contact-form">
+            <input onChange={this.handleChange} value={this.state.name} type="text" name="name" placeholder="Name" />
+            <input onChange={this.handleChange} value={this.state.email} type="email" name="email" placeholder="Enter Email" />
+            <textarea onChange={this.handleChange} value={this.state.message} type="text" name="message" placeholder="Your Message" />
+            <button type="submit" className="button form-button">Submit</button>
+          </form>
+        </div>
+      </section>
+    );
+  }
+  
 }
 
 export default Contact;
