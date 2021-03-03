6e26287eb55e3c9a0a55cb3f689c2dd8ac5f2ae1
Shaun van Ardenne <s.vanardenne@gmail.com>
2021-03-03 14:03:22 -0800

    Added submit handler

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+// Import API configuration
+import apiURL from '../mailAPIconfig';
 
 class Contact extends Component {
 
   // Sets state for controlled inputs
-  state={
+  state= {
     name: "",
     email: "",
     message: "",
@@ -17,12 +21,26 @@ class Contact extends Component {
       [name]: value
     });
   }
+
+  // Handles th submission of form data to mail API
+  handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post(apiURL, this.state).then((res) => {
+      if (res.data.status === 'success') {
+        alert("Message Sent.");
+        this.setState({name: '', email: '', subject: '', message: ''});
+      } else if (res.data.status === 'fail') {
+        alert("Message failed to send.");
+      }
+    });
+  }
+
   render() {
     return(
       <section id="contact" className="main-content contact-wrapper">
       <h2 className="contact-title">Contact</h2>
         <div className="form-wrapper">
-          <form className="contact-form">
+          <form onSubmit={this.handleSubmit} className="contact-form">
             <input onChange={this.handleChange} value={this.state.name} type="text" name="name" placeholder="Name" />
             <input onChange={this.handleChange} value={this.state.email} type="email" name="email" placeholder="Enter Email" />
             <textarea onChange={this.handleChange} value={this.state.message} type="text" name="message" placeholder="Your Message" />
