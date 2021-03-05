d647e05513bf8a705fbea76a62f031f9a12dcf6e
Shaun van Ardenne <s.vanardenne@gmail.com>
2021-03-05 15:30:58 -0800

    Added styling to banner text

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     <header id="home" className="main-header">
       <div className="banner">
         <div className="banner-text">
-          <h1>Hi, I'm Shaun van Ardenne</h1>
+          <h1>Hi, I'm <span>Shaun van Ardenne</span></h1>
           <h1>I'm a full-stack Developer</h1>
         </div>
           <a className="button" href="#details">Check it out!</a>
