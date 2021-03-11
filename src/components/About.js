f7d322950e5b2f91b61967b0c51e32ea0226552e
Shaun van Ardenne <s.vanardenne@gmail.com>
2021-03-11 13:08:30 -0800

    Set up basic layout for about section

--- src/components/About.js
+++ src/components/About.js
@@ -3,51 +3,64 @@ import ProfileImg from '../img/profile/Profile-pic-1.jpg';
 
 const About = (props) => {
   return(
-    <section id="about" className="main-content">
-      <div className="bio-wrapper">
-        <img src={ProfileImg} alt="Picture of Shaun on white background" />
-        <div>
-          <h2>Shaun &mdash; Full-Stack Developer</h2>
-          <p>Lorem ipsum dolor sit amet, 
-          consectetur adipiscing elit, 
-          sed do eiusmod tempor incididunt 
-          ut labore et dolore magna aliqua. 
-          Ut enim ad minim veniam, quis nostrud 
-          exercitation ullamco laboris nisi ut 
-          aliquip ex ea commodo consequat. Duis 
-          aute irure dolor in reprehenderit in 
-          voluptate velit esse cillum dolore eu 
-          fugiat nulla pariatur. Excepteur sint 
-          occaecat cupidatat non proident, sunt in 
-          culpa qui officia deserunt mollit anim id est laborum.</p>
+    <section id="about" className="main-content" style={{position: 'relative'}}>
+      <div className="about">
+        <div className="bio-wrapper">
+          <img src={ProfileImg} alt="Picture of Shaun on white background" />
+          <div>
+            <h2>About Me</h2>
+            <p>Lorem ipsum dolor sit amet, 
+            consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt 
+            ut labore et dolore magna aliqua.</p>
+          </div>
+        </div>
+        <div className="skills">
+          <div className="skill">
+            <div className="skill-title">CSS</div>
+            <div className="skill-percent">
+              <div className="skill-percent-fill" style={{width: '90%'}}></div>
+            </div>
+            <span>90%</span>
+          </div>
+          <div className="skill">
+            <div className="skill-title">HTML</div>
+            <div className="skill-percent">
+              <div className="skill-percent-fill" style={{width: '90%'}}></div>
+            </div>
+            <span>90%</span>
+          </div>
+          <div className="skill">
+            <div className="skill-title">JavaScript</div>
+            <div className="skill-percent">
+              <div className="skill-percent-fill" style={{width: '80%'}}></div>
+            </div>
+            <span>80%</span>
+          </div>
+          <div className="skill">
+            <div className="skill-title">React</div>
+            <div className="skill-percent">
+              <div className="skill-percent-fill" style={{width: '80%'}}></div>
+            </div>
+            <span>80%</span>
+          </div>
+          <div className="skill">
+            <div className="skill-title">Node</div>
+            <div className="skill-percent">
+              <div className="skill-percent-fill" style={{width: '70%'}}></div>
+            </div>
+            <span>70%</span>
+          </div>
+          <div className="skill">
+            <div className="skill-title">UX/UI</div>
+            <div className="skill-percent">
+              <div className="skill-percent-fill" style={{width: '40%'}}></div>
+            </div>
+            <span>40%</span>
+          </div>
         </div>
       </div>
-      <div className="skills">
-        <div className="skill">
-          <div className="skill-name">CSS</div>
-          <div className="percent" style={{width: '90%'}}></div>
-        </div>
-        <div className="skill">
-        <div className="skill-name">HTML</div>
-          <div className="percent" style={{width: '90%'}}></div>
-        </div>
-        <div className="skill">
-        <div className="skill-name">React.js</div>
-          <div className="percent" style={{width: '80%'}}></div>
-        </div>
-        <div className="skill">
-          <div className="skill-name">JavaScript</div>
-          <div className="percent" style={{width: '80%'}}></div>
-        </div>
-        <div className="skill">
-          <div className="skill-name">Node.js</div>
-          <div className="percent" style={{width: '70%'}}></div>
-        </div>
-        <div className="skill">
-          <div className="skill-name">UX/UI</div>
-          <div className="percent" style={{width: '40%'}}></div>
-        </div>
-      </div>
+      
     </section>
   );
 }
