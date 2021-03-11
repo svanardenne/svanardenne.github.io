4cbd427862293d3ec3864f98302095e3d98fe37a
Shaun van Ardenne <s.vanardenne@gmail.com>
2021-03-10 17:08:44 -0800

    Changed text in banner

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
       <div className="banner">
         <div className="banner-text">
           <h1>Hi, I'm <span>Shaun van Ardenne</span></h1>
-          <h1>I'm a full-stack Developer</h1>
+          <h1>I'm a full-stack web developer</h1>
         </div>
           <a className="button" href="#details">Check it out!<FontAwesomeIcon className="banner-icon" icon={['fas', 'arrow-down']} /></a>
       </div>
