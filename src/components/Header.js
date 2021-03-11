35efc44fa6a2c8e05aee7f32b2c5f075f235ccdf
Shaun van Ardenne <s.vanardenne@gmail.com>
2021-03-11 14:08:07 -0800

    Added fade effects on component load using a function from context

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,7 @@ const Header = () => {
           <h1>Hi, I'm <span>Shaun van Ardenne</span></h1>
           <h1>I'm a full-stack web developer</h1>
         </div>
-          <a className="button" href="#details">Check it out!<FontAwesomeIcon className="banner-icon" icon={['fas', 'arrow-down']} /></a>
+          <a className="button" href="#details">See my work<FontAwesomeIcon className="banner-icon" icon={['fas', 'arrow-down']} /></a>
       </div>
     </header>
   );
